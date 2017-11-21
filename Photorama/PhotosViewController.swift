16458e49bd8fd898df644fa3b3ae16847756d46b
Pulck <admin@admin.local>
2017-11-21 18:28:48 +0800

    fix issues

--- Photorama/PhotosViewController.swift
+++ Photorama/PhotosViewController.swift
@@ -16,7 +16,7 @@ class PhotosViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        photoStore.fetchInterestingPhotos()
     }
 
     override func didReceiveMemoryWarning() {
